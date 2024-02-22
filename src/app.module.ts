7b171e86c4b72a20811682ceb39468c471668902
SUJI <qaz8457@naver.com>
2024-02-22 14:36:56 +0900

    typeorm init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './orm.config';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+    AuthModule,
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
